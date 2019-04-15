00047643e58686cf77cb1a05f343861274350afa
Amaury <amaury.einholtz@hotmail.Fr>
2019-04-15 12:09:26 +0200

    + des trucs

---
+++ Crowd Simulation/Assets/Scripts/AgentSpawn.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentSpawn : MonoBehaviour
+{
+    public GameObject male;
+    public GameObject female;
+    public float spawnTime = 0.5f;
+    public int maxAgentNbr = 100;
+    int nbrSpawnedAgents = 0;
+    GameObject spawn1;
+    GameObject spawn2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawn1 = GameObject.Find("Spawner1");
+        spawn2 = GameObject.Find("Spawner2");
+        InvokeRepeating("Spawn", 0.1f, spawnTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Spawn()
+    {
+        if (nbrSpawnedAgents >= maxAgentNbr)
+            return;
+
+        bool spawner = Random.Range(0, 2) == 1 ? true : false;
+        bool character = Random.Range(0, 2) == 1 ? true : false;
+
+        if (spawner)
+        {
+            if (character)
+                Instantiate(male, spawn1.transform.position, spawn1.transform.rotation);
+            else
+                Instantiate(female, spawn1.transform.position, spawn1.transform.rotation);
+        }
+        else
+        {
+            if (character)
+                Instantiate(male, spawn2.transform.position, spawn2.transform.rotation);
+            else
+                Instantiate(female, spawn2.transform.position, spawn2.transform.rotation);
+        }
+
+        nbrSpawnedAgents++;
+    }
+}
---
+++ Crowd Simulation/Assets/Scripts/SpawnWalk.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+// marche pas fdp
+public class SpawnWalk : MonoBehaviour
+{
+
+    private NavMeshAgent agent;
+
+    // for Initialization
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    public void MoveToLocation(Vector3 targetPoint)
+    {
+        agent.destination = targetPoint;
+        agent.isStopped = false;
+    }
+}
