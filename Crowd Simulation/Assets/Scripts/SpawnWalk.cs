3ac187fbda0bb8e75e1a7de20f4bd833df361858
Amaury <amaury.einholtz@hotmail.Fr>
2019-04-17 17:21:25 +0200

    petit travail issou

--- Crowd Simulation/Assets/Scripts/SpawnWalk.cs
+++ Crowd Simulation/Assets/Scripts/SpawnWalk.cs
@@ -5,8 +5,8 @@ using UnityEngine.AI;
 // marche pas fdp
 public class SpawnWalk : MonoBehaviour
 {
-
     private NavMeshAgent agent;
+    private Animator animator;
 
     // for Initialization
     void Awake()
@@ -14,8 +14,24 @@ public class SpawnWalk : MonoBehaviour
         float x = Random.Range(-11f, 10f);
         float z = Random.Range(-6.7f, 6.8f);
 
+        animator = GetComponent<Animator>();
         agent = GetComponent<NavMeshAgent>();
         agent.SetDestination(new Vector3(x, this.gameObject.transform.position.y, z));
+    }
+
+    void Update()
+    {
+        // has reach its final position test
+        if (!agent.pathPending)
+        {
+            if (agent.remainingDistance <= agent.stoppingDistance)
+            {
+                if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f)
+                {
+                    animator.SetTrigger("StopWalk");
+                }
+            }
+        }
     }
 
 }
