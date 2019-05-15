2482c061ef88bd883284844c46ce14d2225377ed
Amaury <amaury.einholtz@hotmail.Fr>
2019-05-15 11:24:37 +0200

    on avance

--- Crowd Simulation/Assets/Scripts/AgentManager.cs
+++ Crowd Simulation/Assets/Scripts/AgentManager.cs
@@ -3,31 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+enum States
+{
+    WAITING = 0,
+    GOINGTOEAT = 1,
+    EATING = 2,
+    TALKING = 3,
+    GOINGTOTALK = 4
+}
+
 // the agent behavior during the party will be managed by a state machine
 public class AgentManager : MonoBehaviour
 {
+    private States currentState;
     private float hunger;
     private float shyness;
     private float moveRate;
-    private bool isGoingtoEat;
+    private float xDest;
+    private float zDest;
     private NavMeshAgent agent;
     private Animator animator;
     private NavMeshObstacle obstacle;
 
     void Start()
     {
+        currentState = States.WAITING;
         obstacle = GetComponent<NavMeshObstacle>();
         animator = GetComponent<Animator>();
         agent = GetComponent<NavMeshAgent>();
         hunger = Random.Range(0.0f, 100.0f);
         shyness = Random.Range(0.0f, 100.0f);
         moveRate = Random.Range(0.0f, 0.001f);
-        isGoingtoEat = false;
     }
 
     void Update()
     {
-        if(animator.GetBool("isWaiting"))
+        if(currentState == States.WAITING)
         {
             float choice = Random.Range(0.0f, 1.0f);
 
@@ -46,13 +57,7 @@ public class AgentManager : MonoBehaviour
                     if (Random.Range(0.0f, 100.0f) < hunger)
                     {
                         // eat
-                        animator.SetBool("isWaiting", false);
-                        animator.Rebind();
-                        animator.SetBool("isWalking", true);
-                        obstacle.enabled = false;
-                        agent.enabled = true;
-                        agent.SetDestination((new Vector3(4.0f, 0.0f, -6.5f)));
-                        isGoingtoEat = true;
+                        SelectTable();
                     }
                     else
                     {
@@ -64,5 +69,67 @@ public class AgentManager : MonoBehaviour
                 }
             }
         }
+        else if(currentState == States.GOINGTOEAT)
+        {
+            CheckReachTable();
+        }
+    }
+
+    void CheckReachTable()
+    {
+        if (agent.enabled)
+        {
+            if (!agent.pathPending && (agent.remainingDistance <= agent.stoppingDistance))
+            {
+                if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f)
+                {
+                    animator.SetBool("isWalking", false);
+                    animator.Rebind();
+                    animator.SetBool("isEating", true);
+                    agent.enabled = false;
+                    obstacle.enabled = true;
+                    currentState = States.EATING;
+                }
+            }
+        }
+    }
+
+    void SelectTable()
+    {
+        animator.SetBool("isWaiting", false);
+        animator.Rebind();
+        animator.SetBool("isWalking", true);
+        obstacle.enabled = false;
+        agent.enabled = true;
+
+        switch(Random.Range(0,5))
+        {
+            case 0:
+                xDest = 4.0f;
+                zDest = -6.5f;
+                break;
+
+            case 1:
+                xDest = -4.0f;
+                zDest = -6.5f;
+                break;
+
+            case 2:
+                xDest = -4.0f;
+                zDest = 6.9f;
+                break;
+
+            case 3:
+                xDest = 4.0f;
+                zDest = 6.9f;
+                break;
+
+            case 4:
+                xDest = 0.0f;
+                zDest = 0.65f;
+                break;
+        }
+        agent.SetDestination((new Vector3(xDest, 0.0f, zDest)));
+        currentState = States.GOINGTOEAT;
     }
 }
